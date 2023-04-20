bf5f19c954941c05338b8e0f40faf76fcd6c4ba4
mlister3 <matthew.list56@outlook.com>
2023-04-20 18:49:17 -0500

    Adding Layers

--- static/js/Logic.js
+++ static/js/Logic.js
@@ -1,9 +1,39 @@
 // Creating the map object
+
+let layers = {
+    PreventativeCare: new L.LayerGroup(),
+    Dental: new L.LayerGroup(),
+    MentalIllness: new L.LayerGroup(),
+    ChronicIllness: new L.LayerGroup(),
+};
+
 let myMap = L.map("map", {
     center: [27, -81.8],
-    zoom: 7
-    });
-  
+    zoom: 7,
+    layers: [
+        layers.PreventativeCare,
+        layers.Dental,
+        layers.MentalIllness,
+        layers.ChronicIllness
+    ]
+});
+
+let overlays = {
+    "Preventative Care": layers.PreventativeCare,
+    "Dental Care": layers.Dental,
+    "Mental Illness": layers.MentalIllness,
+    "Chronic Illness": layers.ChronicIllness
+};
+
+let info = L.control({
+    position: "bottomright"
+});
+
+info.onAdd = function() {
+    let div = L.DomUtil.create("div", "legend");
+    return div;
+  };
+
     // Adding the tile layer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -19,9 +49,6 @@ let myMap = L.map("map", {
 
         // Loop through the data.
         for (let i = 0; i < filteredResponse.length; i++) {
-            console.log(filteredResponse[i].placename);
-
-//            if (filteredResponse[i].placename === 'Fellsmere') {
 
             // Set the data location property to a variable.
             let location = filteredResponse[i].geolocation;
@@ -33,11 +60,20 @@ let myMap = L.map("map", {
                 markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
                     .bindPopup(filteredResponse[i].placename));
             }
-//            }
-  
+
         }
   
-    // Add our marker cluster layer to the map.
+    // Add our marker clusters and layers to the map.
     myMap.addLayer(markers);
-
+    L.control.layers(null, overlays).addTo(myMap);
+    info.addTo(myMap);
     });
+
+function updateLegend(value) {
+    document.querySelector(".legend").innerHTML = [
+        "<p class='PreventativeCare'>Preventative Care: " + value.PreventativeCare + "</p>",
+        "<p class='Dental'>Dental: " + value.Dental + "</p>",
+        "<p class='MentalIllness'>Mental Illness: " + value.MentalIllness + "</p>",
+        "<p class='ChronicIllness'>Chronic Illness: " + value.ChronicIllness + "</p>"
+    ].join("");
+}
