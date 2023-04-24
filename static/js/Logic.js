15638d6a59c8ec32fb9101a4775b66578f2d3d84
Matthew Lister <matthew.list56@outlook.com>
2023-04-24 16:30:31 -0500

    Update Logic.js

--- static/js/Logic.js
+++ static/js/Logic.js
@@ -68,7 +68,7 @@ for (let i = 0; i < filteredResponse.length; i++) {
             + '<br>_____________________________________________'
             + '<br> Total Population: ' + filteredResponse[i].totalpopulation 
             + '<br>'
-            + '<br>' + filteredResponse[i].access2_adjprev + '%' + addDeltaTriangle(filteredResponse[i].access2_adjprev, nationalAvg.access2_adjprev) + '%' + ' Lack of Health Issurance Rate'
+            + '<br>' + filteredResponse[i].access2_adjprev + '%' + addDeltaTriangle(filteredResponse[i].access2_adjprev, nationalAvg.access2_adjprev) + '%' + ' Lack of Health Issurance Rate (RED = Good)'
             + '<br>'
             + '<br> Preventative Care Rates: (RED = Bad)'
             + '<br>' + filteredResponse[i].checkup_adjprev + '%' + addDeltaTriangle(filteredResponse[i].checkup_adjprev, nationalAvg.checkup_adjprev) + '%' + ' Rate of Routine Checkups'
