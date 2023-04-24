c84806a993e99c09c7daab9bff3ff3cdb0931706
Matthew Lister <matthew.list56@outlook.com>
2023-04-24 16:08:09 -0500

    Update Logic.js

--- static/js/Logic.js
+++ static/js/Logic.js
@@ -25,43 +25,79 @@ for (let i = 0; i < filteredResponse.length; i++) {
   
     // Check for the location property.
     if (location) {
+
+    const nationalAvg = {
+        access2_adjprev: 16.23, //
+        checkup_adjprev: 73.26, //
+        cholscreen_adjprev: 85.76, //
+        colon_screen_adjprev: 67.21, //
+        mammouse_adjprev: 73.66, //
+        mhlth_adjprev: 15.62, //
+        depression_adjprev: 21.02, //
+        arthritis_adjprev: 26.68, //
+        binge_adjprev: 16.05, //
+        bphigh_adjprev: 33.97, //
+        cancer_adjprev: 6.8, //
+        casthma_adjprev: 10.2, //
+        obesity_adjprev: 33.72, //
+        kidney_adjprev: 3.32, //
+        highchol_adjprev: 33.7, //
+        diabetes_adjprev: 70,
+        chd_adjprev: 7.26, //
+        copd_adjprev: 8.22, //
+        dental_adjprev: 59.24, //
+        teethlost_adjprev: 16.44, //
+    };
+          
+          // Define a function to add a triangle and delta value to a metric
+    function addDeltaTriangle(value, nationalAvgValue) {
+        const delta = value - nationalAvgValue;
+        let triangle = '';
+        if (delta > 0) {
+            triangle = '<span style="color:green">&#9650;</span>'; // Green up triangle
+        } 
+        else if (delta < 0) {
+            triangle = '<span style="color:red">&#9660;</span>'; // Red downward facing triangle
+        }
+        return triangle + ' ' + Math.abs(delta).toFixed(2);
+        }
   
     // Add a new marker to the cluster group, and bind a popup.
     markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-        .bindPopup(filteredResponse[i].placename + ' (Rate for Adults 18 & Older Unless Specified)'
-            + '<br>________________________________________________'
-            + '<br> Total Population: ' + filteredResponse[i].totalpopulation
-            + '<br> Lack of Health Issurance Rate: ' + filteredResponse[i].access2_adjprev + '%'
+        .bindPopup(filteredResponse[i].placename + '<br>(Percent of Population)(Delta National Average)<br>(Rate for Adults 18 & Older Unless Specified)'
+            + '<br>_____________________________________________'
+            + '<br> Total Population: ' + filteredResponse[i].totalpopulation 
             + '<br>'
-            + '<br> Preventative Care Rates:'
-            + '<br> Rate of Routine Checkups: ' + filteredResponse[i].checkup_adjprev + '%'
-            + '<br> Rate Cholestrol Screenings: ' + filteredResponse[i].cholscreen_adjprev + '%'
-            + '<br> Rate Fecal Occult Blood Tests, Sigmoidoscopy, or Colonoscopy Rate Age 50-75: ' + filteredResponse[i].colon_screen_adjprev + '%'
-            + '<br> Preventative Services Men 65 and Older: ' + filteredResponse[i].corem_adjprev + '%'
-            + '<br> Preventative Services Women 65 and Older: ' + filteredResponse[i].corew_adjprev + '%'
-            + '<br> Mammography use Women 50-74: ' + filteredResponse[i].mammouse_adjprev + '%'
+            + '<br>' + filteredResponse[i].access2_adjprev + '%' + addDeltaTriangle(filteredResponse[i].access2_adjprev, nationalAvg.access2_adjprev) + '%' + ' Lack of Health Issurance Rate'
             + '<br>'
+            + '<br> Preventative Care Rates: (RED = Bad)'
+            + '<br>' + filteredResponse[i].checkup_adjprev + '%' + addDeltaTriangle(filteredResponse[i].checkup_adjprev, nationalAvg.checkup_adjprev) + '%' + ' Rate of Routine Checkups'
+            + '<br>' + filteredResponse[i].cholscreen_adjprev + '%' + addDeltaTriangle(filteredResponse[i].cholscreen_adjprev, nationalAvg.cholscreen_adjprev) + '%' + ' Rate Cholestrol Screenings'
+            + '<br>' + filteredResponse[i].colon_screen_adjprev + '%' + addDeltaTriangle(filteredResponse[i].colon_screen_adjprev, nationalAvg.colon_screen_adjprev) + '%' + ' Rate Fecal Occult Blood Tests, Sigmoidoscopy, or Colonoscopy Rate Age 50-75'
+            + '<br>' + filteredResponse[i].corem_adjprev + '%' + ' Preventative Services Men 65 and Older'
+            + '<br>' + filteredResponse[i].corew_adjprev + '%' + ' Preventative Services Women 65 and Older' 
+            + '<br>' + filteredResponse[i].mammouse_adjprev + '%' + addDeltaTriangle(filteredResponse[i].mammouse_adjprev, nationalAvg.mammouse_adjprev) + '%' + ' Mammography use Women 50-74'
             + '<br>'
-            + '<br> Mental Health Condition Rates:'
-            + '<br> Unwell Mental State for More than 2 Weeks Rate: ' + filteredResponse[i].mhlth_adjprev + '%'
-            + '<br> Depression: ' + filteredResponse[i].depression_adjprev + '%'
+            + '<br> Mental Health Condition Rates: (RED = Good)'
+            + '<br>' + filteredResponse[i].mhlth_adjprev + '%' + addDeltaTriangle(filteredResponse[i].mhlth_adjprev, nationalAvg.mhlth_adjprev) + '%' + ' Unwell Mental State for More than 2 Weeks Rate'
+            + '<br>' + filteredResponse[i].depression_adjprev + '%' + addDeltaTriangle(filteredResponse[i].depression_adjprev, nationalAvg.depression_adjprev) + '%' + ' Depression'
             + '<br>'
-            + '<br> Chronic Illness Rates:'
-            + '<br> Arthritis: ' + filteredResponse[i].arthritis_adjprev + '%'
-            + '<br> Binge Drinking: ' + filteredResponse[i].binge_adjprev + '%'
-            + '<br> High Blood Pressure: ' + filteredResponse[i].bphigh_adjprev + '%'
-            + '<br> Cancer: ' + filteredResponse[i].cancer_adjprev + '%'
-            + '<br> Asthma: ' + filteredResponse[i].casthma_adjprev + '%'
-            + '<br> Obesity: ' + filteredResponse[i].obesity_adjprev + '%'
-            + '<br> Kidney Disease: ' + filteredResponse[i].kidney_adjprev + '%'
-            + '<br> High Cholestrol: ' + filteredResponse[i].highchol_adjprev + '%'
-            + '<br> Diabetes: ' + filteredResponse[i].diabetes_adjprev + '%'
-            + '<br> Coronary Heart Disease: ' + filteredResponse[i].chd_adjprev + '%'
-            + '<br> COPD Rate: ' + filteredResponse[i].copd_adjprev + '%'
+            + '<br> Chronic Illness Rates: (RED = Good)'
+            + '<br>' + filteredResponse[i].arthritis_adjprev + '%' + addDeltaTriangle(filteredResponse[i].arthritis_adjprev, nationalAvg.arthritis_adjprev) + '%' + ' Arthritis'
+            + '<br>' + filteredResponse[i].binge_adjprev + '%' + addDeltaTriangle(filteredResponse[i].binge_adjprev, nationalAvg.binge_adjprev) + '%' + ' Binge Drinking'
+            + '<br>' + filteredResponse[i].bphigh_adjprev + '%' + addDeltaTriangle(filteredResponse[i].bphigh_adjprev, nationalAvg.bphigh_adjprev) + '%' + ' High Blood Pressure'
+            + '<br>' + filteredResponse[i].cancer_adjprev + '%' + addDeltaTriangle(filteredResponse[i].cancer_adjprev, nationalAvg.cancer_adjprev) + '%' + ' Cancer'
+            + '<br>' + filteredResponse[i].casthma_adjprev + '%' + addDeltaTriangle(filteredResponse[i].casthma_adjprev, nationalAvg.casthma_adjprev) + '%' + ' Asthma'
+            + '<br>' + filteredResponse[i].obesity_adjprev + '%' + addDeltaTriangle(filteredResponse[i].obesity_adjprev, nationalAvg.obesity_adjprev) + '%' + ' Obesity'
+            + '<br>' + filteredResponse[i].kidney_adjprev + '%' + addDeltaTriangle(filteredResponse[i].kidney_adjprev, nationalAvg.kidney_adjprev) + '%' + ' Kidney Disease'
+            + '<br>' + filteredResponse[i].highchol_adjprev + '%' + addDeltaTriangle(filteredResponse[i].highchol_adjprev, nationalAvg.highchol_adjprev) + '%' + ' High Cholestrol'
+            + '<br>' + filteredResponse[i].diabetes_adjprev + '%' + addDeltaTriangle(filteredResponse[i].diabetes_adjprev, nationalAvg.diabetes_adjprev) + '%' + ' Diabetes'
+            + '<br>' + filteredResponse[i].chd_adjprev + '%' + addDeltaTriangle(filteredResponse[i].chd_adjprev, nationalAvg.chd_adjprev) + '%' + ' Coronary Heart Disease'
+            + '<br>' + filteredResponse[i].copd_adjprev + '%' + addDeltaTriangle(filteredResponse[i].copd_adjprev, nationalAvg.copd_adjprev) + '%' + ' COPD Rate'
             + '<br>'
-            + '<br> Dental Care Rates:' 
-            + '<br> Regular Dental Visits Rate: ' + filteredResponse[i].dental_adjprev + '%'
-            + '<br> Tooth Loss Rate in Adults 65 and Older: ' + filteredResponse[i].teethlost_adjprev + '%'
+            + '<br> Dental Care Rates: (RED = Good)' 
+            + '<br>' + filteredResponse[i].dental_adjprev + '%' + addDeltaTriangle(filteredResponse[i].dental_adjprev, nationalAvg.dental_adjprev) + '%' + ' Regular Dental Visits Rate'
+            + '<br>' + filteredResponse[i].teethlost_adjprev + '%' + addDeltaTriangle(filteredResponse[i].teethlost_adjprev, nationalAvg.teethlost_adjprev) + '%' + ' Tooth Loss Rate in Adults 65 and Older'
             ));
     }
 
